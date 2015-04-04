116de781d2036e1da7c7ad9d97792e0fd79414a3
jbranchaud <jbranchaud@gmail.com>
2015-04-04 00:29:18 -0500

    Add the git-init function.

--- jenga/src/jenga/core.clj
+++ jenga/src/jenga/core.clj
@@ -2,7 +2,16 @@
   (:gen-class)
   (:require [clojure.java.shell :as shell]))
 
+; TODO: create a mkdirs as necessary function
+
 (defn is-git-repo?
   "Is the directory part of a git repository?"
   [dir]
   (not (= 128 (:exit (shell/sh "git" "status" :dir dir)))))
+
+(defn git-init
+  "Initialize a git repository"
+  [dir]
+  (if (is-git-repo? dir)
+    false
+    (= 0 (:exit (shell/sh "git" "init" :dir dir)))))
--- jenga/test/jenga/core_test.clj
+++ jenga/test/jenga/core_test.clj
@@ -2,7 +2,16 @@
   (:require [clojure.test :refer :all]
             [jenga.core :refer :all]))
 
-(deftest is-a-git-repo?
+(deftest test-is-git-repo?
   (testing "test if a directory is part of a git repo"
     (is (is-git-repo? "/Users/jbranchaud/code/til"))
     (is (not (is-git-repo? "/Users/jbranchaud/code")))))
+
+(deftest test-git-init
+  (testing "test initialization of a git repo"
+    (let [dir "/Users/jbranchaud/.jenga/repos/repo1"]
+      (.mkdirs (java.io.File. dir))
+      (is (git-init dir))
+      (is (is-git-repo? dir)))))
+
+; TODO: clean up repos/dirs after creation
