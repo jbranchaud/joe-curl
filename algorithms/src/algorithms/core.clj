9f2e7e0b967e1f6c8786a71dcb1e2a6111efbd00
jbranchaud <jbranchaud@gmail.com>
2015-04-07 08:44:02 -0500

    Add some algorithms helper methods with tests.

--- algorithms/src/algorithms/core.clj
+++ algorithms/src/algorithms/core.clj
@@ -2,6 +2,38 @@
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  ""
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, Algorithms!"))
+
+; before I knew about the assoc function
+(defn swap-old
+  "Swap items at position i and j"
+  [items i j]
+  (let [x (min i j)
+        y (max i j)
+        a (get items y)
+        b (get items x)]
+    (loop [swapped-list []
+           idx 0
+           [itm & remaining] items]
+      (if (nil? itm)
+        swapped-list
+        (if (= idx x)
+          (recur (conj swapped-list a) (inc idx) remaining)
+          (if (= idx y)
+            (recur (conj swapped-list b) (inc idx) remaining)
+            (recur (conj swapped-list (get items idx)) (inc idx) remaining)))))))
+
+; bound-checking assoc-based swap function
+(defn swap
+  [items i j]
+  (let [size (count items)]
+    (if (or (>= i size) (< i 0) (>= j size) (< j 0))
+      items
+      (assoc items i (items j) j (items i)))))
+
+(defn clone-vector
+  "Clone the given vector"
+  [v]
+  (map identity v))
--- algorithms/test/algorithms/core_test.clj
+++ algorithms/test/algorithms/core_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [algorithms.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest swap-test
+  (testing "Swap items in vec at i and j"
+    (is (= [1 2 3 4] (swap [1 3 2 4] 1 2)))
+    (is (= [5 4 6 7 8] (swap [8 4 6 7 5] 0 4)))
+    (is (= [3 4 5 6] (swap [4 3 5 6] 1 0))))
+  (testing "no swap on bad values of i and j"
+    (is (= [1 2 3 4] (swap [1 2 3 4] 0 5)))
+    (is (= [1 2 3 4] (swap [1 2 3 4] 5 2)))
+    (is (= [1 2 3 4] (swap [1 2 3 4] -1 7)))))
